b501f1b1e002ff3833637bc01d7c89fea77dbaa8
Maja Gacesa <maja.gacesa@ansys.com>
2019-10-15 16:54:55 +0200

    solve exercise 05_01

--- 05_If_statements/activity05_01-comparenumbers.sh
+++ 05_If_statements/activity05_01-comparenumbers.sh
@@ -6,18 +6,19 @@
 echo 'First number: ' $1
 echo 'Second number:' $2
 
-#if [ $1 ] && [ $2 ] && 
+#if [ $1 ] && [ $2 ] &&
 if [ $1 -eq $1 ] && [ $2 -eq $2 ]
 then
-    echo input oook!
-fi
-
-if [ $1 -gt $2 ]
-then
-    echo $1
-elif [ $2 -gt $1 ]
-then
-    echo $2
+    echo input is ok, proceeding with comparison of $1 and $2:
+    if [ $1 -gt $2 ]
+    then
+        echo $1 ' > ' $2
+    elif [ $2 -gt $1 ]
+    then
+        echo $1 ' < ' $2
+    else
+        echo $1 ' = ' $2
+    fi
 else
-    echo 'they are equal'
-fi
+    echo please input two integers.
+fi # end of the input check
